da87ca257b3c89fadf26453599a76ab08e3af7c0
WhiteH26 <131841431+WhiteH26@users.noreply.github.com>
2024-02-15 10:22:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 onload = () => {
   document.body.classList.remove("container");
+
+  const letras = document.getElementById("letras")
+
+  setTimeout(function() {
+    letras.classList.add("hide");
+    
+}, 3000);
 }
